d7e75f86de5e14c2454eded52a851d3a3846dff0
Bram Van Bergen <r0656050@student.thomasmore.be>
2019-07-03 11:21:45 +0200

    Basic routing and 404 error set up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,26 @@
-import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import {NgModule} from '@angular/core';
+import {Routes, RouterModule} from '@angular/router';
+import {LoginComponent} from './login/login.component';
+import {DriversComponent} from './drivers/drivers.component';
+import {CalendarComponent} from './calendar/calendar.component';
+import {OverviewComponent} from './overview/overview.component';
+import {ErrorComponent} from './error/error.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'drivers', component: DriversComponent},
+  {path: 'calendar', component: CalendarComponent},
+  {path: 'overview', component: OverviewComponent},
+  {path: 'login', component: LoginComponent},
+  {path: '404', component: ErrorComponent},
+  // Homepage doorverwijzen naar overview
+  {path: '', redirectTo: 'overview', pathMatch: 'full'},
+  // Niet gedefinieerde routes ook doorverwijzen. Zoniet krijg je fouten in de console!
+  {path: '**', redirectTo: '404'},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
+import {NgBootstrapModule} from './sharedModules/ng-bootstrap.module';
+import { NavbarComponent } from './navbar/navbar.component';
+import { LoginModule } from './login/login.module';
+import { CalendarModule } from './calendar/calendar.module';
+import { DriversModule } from './drivers/drivers.module';
+import { OverviewModule } from './overview/overview.module';
+import { ErrorComponent } from './error/error.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgBootstrapModule,
+    LoginModule,
+    CalendarModule,
+    DriversModule,
+    OverviewModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
